54985e8134381b2aaf46e420ab4ea95c794a5d8a
leclud <negativchikxxl@gmail.com>
2025-03-14 17:43:44 +0500

    task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,19 +5,19 @@ import java.util.stream.IntStream;
 public class Task02Main {
 
     public static void main(String[] args) {
-
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
+    }
 
+    public static int GetGrayCode(int g){
+        return g ^ (g >> 1); // формула с википедии
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
-
+        if (n >= 1 && n <= 16){ // Битность кодов задается аргументом n в пределах от 1 до 16.
+            return IntStream.iterate(0, g -> g + 1).map(g -> GetGrayCode(g % (int)Math.pow(2, n)));
+        }
+        throw new IllegalArgumentException(); // ошибка, если n не лежит в [1, 16]
     }
-
 }
